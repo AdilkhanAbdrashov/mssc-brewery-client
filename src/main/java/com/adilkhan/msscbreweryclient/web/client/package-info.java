635450b0d9cbe6454990b5920d17525c0da6d87d
Adilkhan Abdrashov <67748974+AdilkhanAbdrashov@users.noreply.github.com>
2022-08-07 20:33:14 +0600

    Initial project add; Adding get client; Adding POST example;

---
+++ src/main/java/com/adilkhan/msscbreweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package com.adilkhan.msscbreweryclient.web.client;
---
+++ src/test/java/com/adilkhan/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,40 @@
+package com.adilkhan.msscbreweryclient.web.client;
+
+
+import com.adilkhan.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+
+    }
+}
